4d4ce847f828771a5870efd14ada1dbb907f1fa6
recoo <recoo33@protonmail.com>
2024-05-21 00:07:46 +0300

    SubtitleDataModel

--- api/src/main/java/kurd/reco/api/model/PlayDataModel.kt
+++ api/src/main/java/kurd/reco/api/model/PlayDataModel.kt
@@ -4,10 +4,16 @@ data class PlayDataModel(
     val url: String,
     val title: String?,
     val drm: DrmDataModel?,
-    val subtitles: List<String>?
+    val subtitles: List<SubtitleDataModel>?
 )
 
 data class DrmDataModel(
     val licenseUrl: String,
     val headers: Map<String, String>?
+)
+
+data class SubtitleDataModel(
+    val url: String,
+    val language: String,
+    val id: Any?
 )
