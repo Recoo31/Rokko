53c9678b57a7d79c11c0314b4f0d5112b783b8be
recoo <recoo33@protonmail.com>
2024-05-20 23:48:01 +0300

    Added subtitles to PlayDataModel

--- api/src/main/java/kurd/reco/api/model/PlayDataModel.kt
+++ api/src/main/java/kurd/reco/api/model/PlayDataModel.kt
@@ -3,7 +3,8 @@ package kurd.reco.api.model
 data class PlayDataModel(
     val url: String,
     val title: String?,
-    val drm: DrmDataModel?
+    val drm: DrmDataModel?,
+    val subtitles: List<String>?
 )
 
 data class DrmDataModel(
