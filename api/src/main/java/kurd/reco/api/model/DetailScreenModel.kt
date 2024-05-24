13a64555e2f452ddc1a6df0f961f49db965774f3
recoo <recoo33@protonmail.com>
2024-05-24 22:17:52 +0300

    made description and backImage null-safe

--- api/src/main/java/kurd/reco/api/model/DetailScreenModel.kt
+++ api/src/main/java/kurd/reco/api/model/DetailScreenModel.kt
@@ -2,9 +2,9 @@ package kurd.reco.api.model
 
 data class DetailScreenModel(
     val title: String,
-    val description: String,
+    val description: String?,
     val id: Any,
     val image: String,
-    val backImage: String,
+    val backImage: String?,
     val isSeries: Boolean
 )
